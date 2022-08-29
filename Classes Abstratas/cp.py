58e24718954994ce321eef3c32061af1d464bfa2
antonio <tomgabinio@gmail.com>
2022-08-29 19:34:14 -0300

    .

---
+++ Classes Abstratas/cp.py
@@ -0,0 +1,9 @@
+
+
+# class ContaPoupanca(Conta):
+#     def sacar(self, valor):
+#         if self.saldo < valor:
+#             print("Saldo insuficiente")
+#             return
+
+#         self.sado -= valor
