a8827d19625a7eaa3300c3851bf43adacb80439b
antonio <tomgabinio@gmail.com>
2022-08-30 13:12:55 -0300

    adicionado alguns metodos magicos e criacao da funcao retangulo

--- Sobrecarga de Operadores/main.py
+++ Sobrecarga de Operadores/main.py
@@ -1,4 +1,41 @@
 """
 No python, o comportamento dos operadores é definido por metodos especiais.
 Vamos alterar o comportamento de operadores com classes definidas pelo usuario.
+
+Operador    Método          Operação
+------------------------------------------------------
++           __add__         adição
+-           __sub__         subtração
+*           __mul__         multiplicação
+/           __div__         divisão
+//          __floordiv__    divisão inteira
+%           __mod__         Módulo
+**          __pow__         Potência
++           __pos__         Positivo
+-           __neg__         Negativo
+<           __lt__          Menor que
+>           __gt__          Maior que
+<=          __le__          Menor ou igual a
+>=          __ge__          Maior ou igual a
+==          __eq__          Igual a
+!=          __ne__          Diferente de
+<<          __lshift__      Deslocamento para a esquerda
+>>          __rshift__      Deslocamento para a direita
+&           __and__         E bit-a-bit
+|           __or__          OU bit-a-bit
+^           __xor__         OU exclusivo bit-a-bit
+~           __inv__         inversão
+
 """
+
+
+class Retangulo:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+r1 = Retangulo(10, 20)
+r2 = Retangulo(10, 20)
+
+print(r1 + r2)
