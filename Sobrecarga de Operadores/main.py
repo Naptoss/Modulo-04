d2febf7fafca9cfdbe129eac477d93d88c915bff
antonio <tomgabinio@gmail.com>
2022-08-30 18:08:41 -0300

    adicionado a função de soma e a função que verifica na memoria a soma dos valores em conjunto

--- Sobrecarga de Operadores/main.py
+++ Sobrecarga de Operadores/main.py
@@ -34,8 +34,16 @@ class Retangulo:
         self.x = x
         self.y = y
 
+    def __repr__(self):
+        return f"<class 'Retangulo({self.x}, {self.y})>"
+
+    def __add__(self, other):
+        novo_x = self.x + other.x
+        novo_y = self.y + other.y
+        return Retangulo(novo_x, novo_y)
+
 
 r1 = Retangulo(10, 20)
 r2 = Retangulo(10, 20)
-
-print(r1 + r2)
+r3 = r1 + r2
+print(r3)
