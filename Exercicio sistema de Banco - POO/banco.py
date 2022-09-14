53ad1fa482ae739344fd191ef8768be04569d785
antonio <tomgabinio@gmail.com>
2022-09-13 21:30:29 -0300

    adiconado a agregação de contas e clientes

--- Exercicio sistema de Banco - POO/banco.py
+++ Exercicio sistema de Banco - POO/banco.py
@@ -2,3 +2,11 @@ class Banco:
     def __init__(self, contas, cliente):
         self._contas = contas
         self._cliente = cliente
+
+    @property
+    def contas(self):
+        return self._contas
+
+    @property
+    def cliente(self):
+        return self._cliente
