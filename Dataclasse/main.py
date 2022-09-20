1b455cddf8b4a7a9ea1a9557936ae903d729318a
antonio <tomgabinio@gmail.com>
2022-09-20 17:18:20 -0300

    O que é Datac;asses

--- Dataclasse/main.py
+++ Dataclasse/main.py
@@ -1,3 +1,11 @@
+"""O módulo Dataclasses fornece um decorador e funções
+para criar automaticamente métodos, como __init__(), __repr__(), __eq__ (etc)
+em classes definidas pelo usuário.
+Basicamente, dataclasses são syntax sugar para criar classes normais.
+Foi originalmente descrito na PEP 557.
+Adicionado na versão 3.7 do Python."""
+
+
 from dataclasses import dataclass
 
 
